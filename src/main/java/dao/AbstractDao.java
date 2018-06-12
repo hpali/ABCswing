b8c4cfc8d5a12ad7eb2416939acb29d7b31c1450
hpali <hidy.pal@gmail.com>
2018-06-12 14:36:42 +0200

    Távoli adatbázis url megadása

--- src/main/java/dao/AbstractDao.java
+++ src/main/java/dao/AbstractDao.java
@@ -8,6 +8,8 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import util.Utility;
 
 /**
@@ -20,6 +22,15 @@ public abstract class AbstractDao implements IDao {
     private Connection connection;
 
     public AbstractDao(String databaseurl, String username, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AbstractDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(AbstractDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(AbstractDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
         dbConfig = new DbConfig(databaseurl, username, password);
     }
 
@@ -29,12 +40,12 @@ public abstract class AbstractDao implements IDao {
 
     public Connection getConnection() {
         try {
+
             connection = DriverManager.getConnection(dbConfig.getDatabaseurl(), dbConfig.getUsername(), dbConfig.getPassword());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return connection;
     }
-    
-    
+
 }
